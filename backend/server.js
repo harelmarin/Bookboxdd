97a7d3f1fbc3aa816f316ea4bc27b486eeceb5b4
harelmarin <145020856+harelmarin@users.noreply.github.com>
2024-08-05 10:59:21 +0200

    Add : base react

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use (cors());
+
+mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Boobox'
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(8001, () => {
+
+    console.log('Server is running on port 8001');
+});
---
+++ frontend/src/components/home.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+
+function Home() {
+  return (
+    <div>
+        TEST HOME
+      
+    </div>
+  );
+}
+
+export default Home;
